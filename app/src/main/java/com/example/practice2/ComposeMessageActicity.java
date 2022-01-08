7b9b7680576d799a49377ffff6a4e62843ab41bc
Anthony Kim <fbssaejun@gmail.com>
2022-01-08 17:23:04 -0500

    add buttons on main activity, add new activity that compose message

--- app/src/main/java/com/example/practice2/ComposeMessageActicity.java
+++ app/src/main/java/com/example/practice2/ComposeMessageActicity.java
@@ -4,15 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ComposeMessageActicity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void composeMessage {
-
+        setContentView(R.layout.activity_compose_message_acticity);
     }
 }
